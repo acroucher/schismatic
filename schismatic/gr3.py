817bed83b868172b8eb2ccf06ac9a3bf20c06f7d
Adrian Croucher <a.croucher@auckland.ac.nz>
2023-08-25 14:51:10 +1200

    Add license info to source files

--- schismatic/gr3.py
+++ schismatic/gr3.py
@@ -1,3 +1,13 @@
+"""Copyright 2023 University of Auckland.
+
+This file is part of schismatic.
+
+schismatic is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+schismatic is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with schismatic.  If not, see <http://www.gnu.org/licenses/>."""
+
 """SCHISM gr3 grids."""
 
 import os
