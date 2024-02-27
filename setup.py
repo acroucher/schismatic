d4ee2aeee7afb4fd27189926e120a3635ee8bbea
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-02-27 14:50:57 +1300

    Move to pyproject.toml for packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-import setuptools
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name='schismatic',
-    version='0.0.1',
-    description='Library for pre- and post-processing of SCHISM simulations',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author='Adrian Croucher',
-    author_email='a.croucher@auckland.ac.nz',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"],
-    python_requires='>=2.7',
-    install_requires=['numpy', 'scipy', 'pyproj', 'netCDF4', 'meshio',
-                      'matplotlib', 'vtk']
-)
+from setuptools import setup
+# for backwards compatibility only
+setup()
